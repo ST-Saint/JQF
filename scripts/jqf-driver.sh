ab04188f51563203afdbcca1c42bb080c82c6c0a
ST-Saint <st.saint.wyy@gmail.com>
2021-11-11 01:41:25 +0800

    debug: test jacoco

--- scripts/jqf-driver.sh
+++ scripts/jqf-driver.sh
@@ -16,10 +16,12 @@ INST_DIR="${ROOT_DIR}/instrument/target/"
 
 FUZZ_JAR="${FUZZ_DIR}/$project-fuzz-$version.jar"
 INST_JAR="${INST_DIR}/$project-instrument-$version.jar"
+# INST_JAR="/home/yayu/Downloads/jacoco/lib/jacocoagent.jar=destfile=/home/yayu/tmp/hadoop/hadoop-jacoco.exec,classdumpdir=/home/yayu/hadoop/hadoop-class/,output=file,address=localhost"
 
 # Compute classpaths (the /classes are only for development; 
 #   if empty the JARs will have whatever is needed)
 INST_CLASSPATH="${INST_DIR}/classes:${INST_JAR}:${INST_DIR}/dependency/asm-9.1.jar"
+# INST_CLASSPATH=""
 FUZZ_CLASSPATH="${FUZZ_DIR}/classes:${FUZZ_JAR}"
 
 # If user-defined classpath is not set, default to '.'
